e8e939d44408dddc84042e43eb48448732fe0c1a
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-05-03 23:55:19 +0300

    added template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import menuItemsTemplate from './menu-item.hbs';
+import menuItems from './menu.json';
 import './styles.css';
 
+// Тема
 const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
 };
+
+// Шаблонизация
+const menuContainer = document.querySelector('.js-menu');
+const menuMarkup = createMenuItems(menuItems);
+
+menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
+
+function createMenuItems(menuItems) {
+    return menuItemsTemplate(menuItems);
+}
